fa2cbde39a9d041bc116445f0d5787c3586071be
Timo Kock <timo.kock@movicoders.com>
2021-05-11 10:45:08 +0200

    app chnages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ function App() {
     <div className="container mt-5">
      <h1>Tareas</h1>
      <hr/>
+     <div className="row">
+       <div className="col-8">
+        <h4 className="text-center">Lista de Tareas</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <span className="lead">Nombre de la tarea</span>
+            <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+            <button className="btn btn-warning btn-sm float-right">Editar</button>
+          </li>
+        </ul>
+
+       </div>
+       <div className="col-4">
+        <h4 className="text-center">Formulario</h4>
+        <form action="">
+          <input type="text" className="form-control mb-2" placeholder="Ingrese la tarea..."/>
+          <button type="submit" className="btn btn-success btn-sm float-right">Agregar</button>
+        </form>
+       </div>
+     </div>
     </div>
   )
 }
